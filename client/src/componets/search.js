e750b0662de19950a557c617c08f9bb402edf6e0
zhen yong chen <dannychen111@yahoo.com>
2020-03-03 00:00:36 -0600

    able to get the results from the google books api

--- client/src/componets/search.js
+++ client/src/componets/search.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SearchBar from './Searchbar';
+import Axios from 'axios';
 class Search extends Component{
     state={
         search: "",
@@ -12,8 +13,18 @@ class Search extends Component{
           [name]: value
         });
     }
+    // :keyes&key=AIzaSyA7mmY3NZcjuqBUI79Fnscy_ps80-4h2-Y
     handleSubmit = () => {
-
+        Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${encodeURI(this.state.search)}`)
+            .then((result) => {
+                console.log(result.data.items);
+                this.setState({
+                    result: result.data.items
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
     render(){
