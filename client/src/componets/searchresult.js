c52cad7cdadf4da832bf8b33c3e875c34917df76
zhen yong chen <dannychen111@yahoo.com>
2020-03-03 23:50:55 -0600

    react frontend is finished just need to add api

--- client/src/componets/searchresult.js
+++ client/src/componets/searchresult.js
@@ -1,22 +1,31 @@
 import React from 'react';
 
 
-function Results(props){  
+function Results(props) {
     return (
-        <div style={{width:"100%", border:"solid 2px black", margin:"30px 10px 0px 10px", padding: "10px"}}>
+        <div style={{ width: "100%", border: "solid 2px black", margin: "30px 10px 0px 10px", padding: "10px" }}>
             <h3>Results :</h3>
             {props.result.map(res => (
-                <div style={{width:"100%", border:"solid 1px black"}}>
-                    title {res.title}
-                    <br/>
-                    authors {res.authors}
-                    <br/>
-                    discription{res.description}
-                    <br/>
-                    <img src={res.image} alt={res.title} />
-                    <br/>
-                    <br/>
-                    <a href={res.link} >the link</a>
+                <div style={{ width: "100%", border: "solid 1px black", padding: "10px" }}>
+                    <div className="row">
+                        <div className="col-md-9">
+                            <b>{res.title}</b>
+                            <br />
+                            <p>authors: {res.authors ? res.authors : "null"}</p>
+                        </div>
+                        <div className="col-md-3" style={{textAlign:"right"}}>
+                            <a className="btn btn-info" href={res.link} >More info</a>
+                            <a onClick={props.onSave} style={{color: "white"}} className="btn btn-success">Save</a>
+                        </div>
+                    </div>
+                    <div className="row" >
+                        <div className="col-md-3">
+                            <img src={res.image} alt={res.title} />
+                        </div>
+                        <div className="col-md-9">
+                            description: <br /> {res.description ? res.description : "null"}
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
