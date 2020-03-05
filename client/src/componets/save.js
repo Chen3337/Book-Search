320071c8f8c94da6cc536a021f026815a60050f7
zhen yong chen <dannychen111@yahoo.com>
2020-03-04 22:53:05 -0600

    delete route and delete function working finish for now

--- client/src/componets/save.js
+++ client/src/componets/save.js
@@ -1,23 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import Savedbooks from './savedbooks';
 class Save extends Component {
     state = {
         saveBooks: []
     }
-    componentDidMount(){
+    componentDidMount() {
+        this.getSavedBooks();
+    }
+    getSavedBooks = () => {
         axios.get('/api/savedbook')
             .then((response) => {
                 this.setState({
-                    saveBooks : response.data
+                    saveBooks: response.data
                 })
             }).catch((err) => console.log(err));
     }
     deleteSaved = (id) => {
-        console.log(id);
+        axios.delete(`/api/deletebook/${id}`)
+            .then((response) => {
+                this.getSavedBooks();
+            }).catch((err) => console.log(err));
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Savedbooks saveBooks={this.state.saveBooks} deleteSaved={this.deleteSaved} />
             </div>
--- route/route.js
+++ route/route.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const books = require('../model/book');
 
-router.post('/newbook', function (req, res){
+router.post('/newbook', function (req, res) {
     var book = req.body;
     books.create({
         title: book.title,
-        authors: JSON.stringify(book.authors) ,
+        authors: JSON.stringify(book.authors),
         description: book.description,
         image: book.image,
         link: book.link
@@ -15,12 +15,17 @@ router.post('/newbook', function (req, res){
     }).catch((err) => res.send(err));
 });
 
-router.get('/savedbook', function (req, res){
+router.get('/savedbook', function (req, res) {
     books.find({}).then((result) => {
         res.json(result);
     }).catch((err) => res.send(err));
-})
+});
 
-
+router.delete('/deletebook/:id', function (req, res) {
+    var id = req.params.id;
+    books.findByIdAndDelete(id).then((result) => {
+        res.json(result);
+    }).catch((err) => res.send(err));
+});
 
 module.exports = router;
